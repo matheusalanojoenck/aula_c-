c788b57459791faf80a72c22b848f6a16790b21b
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2020-04-21 23:42:14 -0300

    primeira versão pronta

--- Aula 1/cpf.c
+++ Aula 1/cpf.c
@@ -13,51 +13,110 @@ typedef struct {
 int check_digitos(char *cpf){
   int first_digit = cpf[CPF_SIZE-2] - '0';
   int second_digit = cpf[CPF_SIZE-1] - '0';
+
+  int aux_first_digit, aux_second_digit;
   int aux_soma = 0, aux_mult = 10;
+  int resto;
+
+  //----VERIFICANDO PRIMEIRO DIGITO-------
   for (int i = 0; i < CPF_SIZE-2; i++)
   {
-    printf("%d * %d\n", cpf[i] - '0', aux_mult);
     aux_soma += (cpf[i]-'0')*aux_mult;
     aux_mult--;
   }
+
+  resto = aux_soma % 11;
+
+  //Calcula qual deveria ser o primeiro digito
+  if(resto < 2){
+    aux_first_digit = 0;
+  }else{
+    aux_first_digit = 11 - resto;
+  }
+
+  //Reforna falso caso o primeiro digito for diferente da verificação
+  if(aux_first_digit != first_digit) return 0;
   
+  //----VERIFICANDO SEGUNDO DIGITO-------
+  aux_soma = 0;
+  aux_mult = 11;
 
+  for (int i = 0; i < CPF_SIZE-1; i++)
+  {
+    aux_soma += (cpf[i]-'0')*aux_mult;
+    aux_mult--;
+  }
 
-  printf("1 digito: %d | 2 digito: %d\n", first_digit, second_digit);
-  printf("\n%d\n", aux_soma);
+  resto = aux_soma % 11;
+
+  //Calcula qual deveria ser o segundo digito
+  if(resto < 2){
+    aux_second_digit = 0;
+  }else{
+    aux_second_digit = 11 - resto;
+  }
+
+  //Reforna falso caso o segundo digito for diferente da verificação
+  if(aux_second_digit != second_digit) return 0;
+
+  return 1;
+}
+
+//Verifica para CPF com todos os numeros iguais
+int isSingleDigit(const char *cpf)
+{
+  int singleDigit = 1;
+  ++cpf;
+  do {
+	singleDigit = singleDigit && (*(cpf - 1) == *cpf);
+	++cpf;
+  } while (*cpf && singleDigit);
+  return singleDigit;
 }
 
 //Retorna 1 se o CPF for valido. Retorna 0 se o CPF não forma valido
 int valida_cpf(char *cpf){
-  if(cpf == NULL) return 0;
-  if (strlen(cpf) != CPF_SIZE) return 0;
+  if(cpf == NULL)
+  {
+    return 0;
+  } 
+  if (strlen(cpf) != CPF_SIZE)
+  {
+    return 0;
+  }
 
-  check_digitos(cpf);
-  
+  if (isSingleDigit(cpf)) {
+	  return 0;
+  } 
+
+  return check_digitos(cpf);
 }
-
 
 void mostra_pessoa(Pessoa p){
   printf("Nome: %s \nIdade: %hu \nCPF: %s", p.nome, p.idade ,p.cpf);
 }
 
 int main(int argc, char const *argv[]) {
-  Pessoa pessoa;
 
-  //printf("Nome: ");
-  //scanf("%s", pessoa.nome);
-  printf("CPF: ");
-  scanf("%s", pessoa.cpf);
-  //printf("Idade: ");
-  //scanf("%hu", &pessoa.idade);
+  int qnt = 5;
+  Pessoa pessoa[qnt];
 
-  //mostra_pessoa(pessoa);
-
-  //strcpy(pessoa.cpf, "008185736944");
-
-  valida_cpf(pessoa.cpf);
-
-  //23231517117
+  //Leitura de dados
+  for (int i = 0; i < qnt; i++)
+  {
+    printf("Nome: ");
+    scanf("%s", pessoa[i].nome);
+    printf("Idade: ");
+    scanf("%hu", &pessoa[i].idade);
+    printf("CPF: ");
+    scanf("%s", pessoa[i].cpf);
+    while (valida_cpf(pessoa[i].cpf) == 0)
+    {
+      printf("CPF invalida! Digite novamente!\n");
+      printf("CPF: ");
+      scanf("%s", pessoa[i].cpf);
+    }
+  }
 
   return 0;
 }
