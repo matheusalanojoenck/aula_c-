99ad58b02e401ce6fc3a9b8d2eeb40fca88769c3
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2020-04-24 18:34:08 -0300

    Update main.cpp Aula 3

--- aula_3/main.cpp
+++ aula_3/main.cpp
@@ -4,41 +4,6 @@
 
 int main(int argc, char const *argv[]) 
 {
-  // std::cout << "Olarrrr" << std::endl;
-
-  // char meuChar;
-  // int meuInt;
-  
-  // std::cout << "Digite um char e um int: ";
-  // std::cin >> meuChar >> meuInt;
-  // std::cout << meuChar << " " << meuInt << std::endl;
-
-  // Pessoa p1, p2;
-  // std::cin >> p1.cpf >> p2.cpf;
-  // std::cout << p1.cpf << "\t"<< p2.cpf << std::endl;
-
-  // Pessoa pessoa;
-
-  // std::cout << "Pessoa" << std::endl;
-  // std::cout << "Nome: ";
-  // std::cin >> pessoa.nome;
-  // std::cout << "Idade: ";
-  // std::cin >> pessoa.idade;
-  // std::cout << "CPF: ";
-  // std::cin >> pessoa.cpf;
-
-  // std::cout << pessoa.nome << "\t" << pessoa.idade << "\t" << pessoa.cpf << std::endl;
-
-  // std::cout << "--------------------" << std::endl;
-  // Retangulo retangulo;
-
-  // std::cout << "Retangulo" << std::endl;
-  // std::cout << "Altura(cm): ";
-  // std::cin >> retangulo.altura;
-  // std::cout << "Largura(cm): ";
-  // std::cin >> retangulo.largura;
-  // std::cout << retangulo.altura << "\t" << retangulo.largura << std::endl;
-
   Pessoa p1;
   unsigned long cpf = 123;
   unsigned short idade = 23;
