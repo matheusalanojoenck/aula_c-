e1298d6f592b876eb936533352e2bd8725a3c9c5
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2020-05-06 17:01:40 -0300

    Update Disciplina.cpp

--- aula_8/Disciplina.cpp
+++ aula_8/Disciplina.cpp
@@ -11,7 +11,7 @@ Pessoa** Disciplina::getVetorAlunos(){
 }
 
 bool Disciplina::adicionarAluno(Pessoa* aluno){
-	if(numAlunos > 50){
+	if(numAlunos >= 50){
 		return false;
 	} else{
 		alunos[numAlunos] = aluno;
