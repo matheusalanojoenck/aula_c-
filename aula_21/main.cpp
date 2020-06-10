c342f5ed06534272c2fc95b1a38f391c421026a8
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2020-06-10 17:18:56 -0300

    codigo aula 21

---
+++ aula_21/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "Pilha.hpp"
+
+int main(){
+	int retorno;
+	Pilha<> p;
+	p.push(1);
+	p.push(2);
+	p.push(3);
+	p.push(4);
+
+	while(!p.estaVazia()){
+		p.pop(&retorno);
+		std::cout << retorno << std::endl;
+	}
+	
+	std::cout << "Fim" << std::endl;
+
+	return 0;
+}
